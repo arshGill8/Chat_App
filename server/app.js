46232d8569669620e3bfc344d28b9261e0b37fe5
arshgill8 <arsh95g@gmail.com>
2020-06-25 12:55:08 -0400

    hi

--- server/app.js
+++ server/app.js
@@ -3,18 +3,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// create web socket server
-const port = process.env.PORT || 8989;
-const wss = new WebSocket.Server({ port });
-
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("build"));
+  app.use(express.static("public/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
   });
 }
+
+const port = process.env.PORT || 8989;
+// create web socket server
+
+const wss = new WebSocket.Server({ port });
 
 // create array to hold users
 const users = [];
