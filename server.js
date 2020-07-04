f7b46d226f3b694122b0cd88d4e61073dd8b5ee2
arshgill8 <arsh95g@gmail.com>
2020-07-04 19:06:13 -0400

    hi

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const WebSocket = require("ws");
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-
-const wss = new WebSocket.Server({ port: port});
+const wss = new WebSocket.Server({ PORT });
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-  
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"));
   });
